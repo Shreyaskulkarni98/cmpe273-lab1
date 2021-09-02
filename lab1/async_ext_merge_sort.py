a58df6410e8ac3d59346c7240a95074a0cbc37d6
Shreyas Kulkarni <shreyasrameshkulkarni@gmail.com>
2021-09-01 23:11:31 -0700

    Re-wrote the merge function to remove manual merging

--- lab1/async_ext_merge_sort.py
+++ lab1/async_ext_merge_sort.py
@@ -1,4 +1,5 @@
 import asyncio
+import heapq
 import os
 from heapq import merge
 
@@ -43,28 +44,24 @@ async def get_sorted_lists():
 
     await asyncio.gather(async_tracker[0], async_tracker[1], async_tracker[2], async_tracker[3], async_tracker[4], async_tracker[5], async_tracker[6], async_tracker[7], async_tracker[8], async_tracker[9])
 
+
 def write_to_file(listname, output_filename):
     with open('output/' + output_filename, mode='wt') as fout:
         for nums in listname:
             fout.write('%s \n' % nums)
         fout.close()
 
+
+def merge(*list):
+    return heapq.merge(*list)
+
+
 def main():
     # 1. open, run and sort all the files in async
     asyncio.run(get_sorted_lists())
 
     # 2. merge
-    list01 = list(merge(nums_listoflist[0], nums_listoflist[1]))
-    list23 = list(merge(nums_listoflist[2], nums_listoflist[3]))
-    list45 = list(merge(nums_listoflist[4], nums_listoflist[5]))
-    list67 = list(merge(nums_listoflist[6], nums_listoflist[7]))
-    list89 = list(merge(nums_listoflist[8], nums_listoflist[9]))
-
-    list03 = list(merge(list01, list23))
-    list47 = list(merge(list45, list67))
-    list07 = list(merge(list03, list47))
-
-    finallist = list(merge(list07, list89))
+    finallist = list(merge(*nums_listoflist))
 
     # 3.writing to file
     write_to_file(finallist, 'async_all_sorted_.txt')
--- lab1/ext_merge_sort.py
+++ lab1/ext_merge_sort.py
@@ -1,3 +1,4 @@
+import heapq
 import os
 from heapq import merge
 
@@ -21,6 +22,9 @@ def write_to_file(listname, output_filename):
         fout.close()
 
 
+def merge(*list):
+    return heapq.merge(*list)
+
 def main():
     # 1.sorting
     unsorted_filename: str
@@ -31,17 +35,7 @@ def main():
             continue
 
     # 2.merging
-    list01 = list(merge(nums_listoflist[0], nums_listoflist[1]))
-    list23 = list(merge(nums_listoflist[2], nums_listoflist[3]))
-    list45 = list(merge(nums_listoflist[4], nums_listoflist[5]))
-    list67 = list(merge(nums_listoflist[6], nums_listoflist[7]))
-    list89 = list(merge(nums_listoflist[8], nums_listoflist[9]))
-
-    list03 = list(merge(list01, list23))
-    list47 = list(merge(list45, list67))
-    list07 = list(merge(list03, list47))
-
-    finallist = list(merge(list07, list89))
+    finallist = list(merge(*nums_listoflist))
 
     # 3.writing to file
     write_to_file(finallist, 'all_sorted_.txt')
